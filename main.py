0a888e88a28ef129655b532a6115d1e775268b21
Romuru <pastelblue0721@gmail.com>
2022-05-18 00:00:07 +0900

    before write code

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("test test 1 ")
+def is_on_list(a, b):
+    return null
+
+
+def get_x(a, b):
+    return null
+
+
+def add_x(a, b):
+    return null
+
+
+def remove_x(a, b):
+    return null
+
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
